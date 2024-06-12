2b3fe4dc84299172806ffc4f40ff60f8d477f756
daniel-z-johnson <danzaljohnson@gmail.com>
2024-06-12 01:31:36 -0500

    seems to work as intended

--- main.go
+++ main.go
@@ -1,27 +1,51 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"image/png"
+	"net/http"
 	"os"
+	"path"
 	"regexp"
+
+	"image"
+	_ "image/gif"
+	_ "image/jpeg"
 )
-import "flag"
 
 func main() {
 	fmt.Println("Program Start")
-	var folder = flag.String("f", "", "foldername")
+	folder := flag.String("f", "", "folder name")
+	filename := flag.String("name", "page_%04d", "")
+	downloadURL := flag.String("loc", "", "URL template for the downloads")
+	start := flag.Int("start", 0, "start count")
+	end := flag.Int("end", 0, "end count")
 	flag.Parse()
 
-	fmt.Println("'" + *folder + "'")
+	fmt.Println("Config being used")
+	fmt.Printf("folder: '%s'\n", *folder)
+	fmt.Printf("filename: '%s'\n", *filename)
+	fmt.Printf("downlaodURL: '%s'\n", *downloadURL)
+	fmt.Printf("start: '%d'\n", *start)
+	fmt.Printf("end: '%d'\n", *end)
 
 	if checkName(*folder) != nil {
 		panic("folder name is invalid")
 	}
-
-	err := os.MkdirAll("downloads/"+*folder, 0755)
+	folderPath := path.Join("downloads", *folder)
+	err := os.MkdirAll(folderPath, 0755)
 	if err != nil {
 		panic(err)
 	}
+
+	for i := *start; i <= *end; i++ {
+		err = downloadImage(folderPath, fmt.Sprintf(*filename, i), fmt.Sprintf(*downloadURL, i))
+		if err != nil {
+			fmt.Printf("Err downloading img: '%s'", err)
+		}
+	}
+
 	fmt.Println("Finished")
 }
 
@@ -35,5 +59,28 @@ func checkName(name string) error {
 		return fmt.Errorf("foldername is invlid")
 	}
 
+	return nil
+}
+
+func downloadImage(folderPath, filename, downloadURL string) error {
+	filePath := path.Join(folderPath, filename)
+	f1, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer f1.Close()
+	resp, err := http.Get(downloadURL)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("did not get 200 status code but got %d", resp.StatusCode)
+	}
+	defer resp.Body.Close()
+	img, _, err := image.Decode(resp.Body)
+	if err != nil {
+		return err
+	}
+	png.Encode(f1, img)
 	return nil
 }
